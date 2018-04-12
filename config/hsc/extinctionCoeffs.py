98c39041b3213979c7ed7f7a0c90fec8c2804ba5
Lauren MacArthur <lauren@astro.princeton.edu>
2018-04-12 15:14:09 -0400

    Add config/hsc/extinctionCoeffs.py
    This adds the extinction coefficients for HSC filters for conversion from E(B-V)
    to extinction, A_filter as an obs_subaru config.  The numbers were provided by
    Masayuki Tanaka (see

---
+++ config/hsc/extinctionCoeffs.py
@@ -0,0 +1,15 @@
+# Extinction coefficients for HSC filters for conversion from E(B-V) to extinction, A_filter.
+# Numbers provided by Masayuki Tanaka #subaru-hsc (see
+# https://lsstc.slack.com/archives/C2JPXB4HG/p1518715246000089)
+#
+# Band, A_filter/E(B-V)
+config.extinctionCoeffs = {
+    "HSC-G": 3.240,
+    "HSC-R": 2.276,
+    "HSC-I": 1.633,
+    "HSC-Z": 1.263,
+    "HSC-Y": 1.075,
+    "NB0387": 4.007,
+    "NB0816": 1.458,
+    "NB0921": 1.187,
+}
