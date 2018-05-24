084dd2bba252c9a5c4f0944e3c4f150c57b5e832
sutieng <30295725+sutieng@users.noreply.github.com>
2018-05-24 21:57:11 +0100

    Add files via upload

--- bin.src/superbitIngestImages.py
+++ bin.src/superbitIngestImages.py
@@ -1,3 +1,3 @@
-#!/Users/sutieng/Documents/lsst_stack/python/miniconda3-4.2.12/bin/python
+#!/Users/ieng/Documents/lsst_stack/python/miniconda3-4.3.21/bin/python
 from lsst.obs.superBIT.ingestsuperbit import SuperBITIngestTask
 SuperBITIngestTask.parseAndRun()
